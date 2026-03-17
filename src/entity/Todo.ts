8d9142fb943f272ea24b8eb4f50d103cad5ab423
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-17 20:06:23 +0500

    Add src/entity/Todo.ts

---
+++ src/entity/Todo.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('todos')
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'text', nullable: false })
+  title!: string;
+
+  @Column({ type: 'boolean', default: false })
+  completed!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
